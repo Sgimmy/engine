65c718d769d4ec8e9bcb3cc452c2d98b7871be23
Simone Gizzi <simonegizzi85@gmail.com>
2020-11-17 11:58:25 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import configuration from './configuration';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+  // app.use(compression());
+  // app.use(helmet());
+  // app.use(csurf());
+  // app.use(rateLimit(configuration().security.rateLimit));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true
+  }));
+  await app.listen(configuration().port);
 }
 bootstrap();
