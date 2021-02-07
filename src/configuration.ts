8d6cd18df71fea95b5863cf3f4e95e9de20f3239
Mario Pacella <pacellamario@gmail.com>
2021-02-07 12:08:27 +0100

    fix port in configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -28,7 +28,7 @@ export default () => {
     },
     database: {
       host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+      port: process.env.PORT || 5000,
       name: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       pass: process.env.DATABASE_PASS
